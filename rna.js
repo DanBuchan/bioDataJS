8dafc31fd22d59c53211fe8cd306a56d6ff63db1
Daniel Buchan <clone@myself.com>
2021-09-17 13:52:11 +0100

    moved mrna class to rna to make it more generic

---
+++ rna.js
@@ -0,0 +1,65 @@
+/*jshint esversion: 6 */
+let protein = require('./protein.js');
+let sequence = require('./sequence.js');
+
+// nrna_sequence : an rna nucleotide sequence or a sequence object of
+//                 type nucleotide creadted by db.sequence()
+// protein : a protein aa sequence or a sequence object of
+//           type aminoacid creadted by db.sequence()
+// annoations: an objects of key:value pairs. Of annotations for the rna
+// source: A strong the defines where the data revord came from (i.e. a file
+//         name or URI
+// type: default type of rna is mrna, overrrde for other classes
+const rna = function(rna_sequence, rna_protein={}, annotations={}, source='', type="mrna") {
+
+  if(typeof rna_protein === 'string' || rna_protein instanceof String)
+  {//if we got a string try and make a sequence out of it
+    rna_protein = protein.protein(rna_protein);
+  }
+  else
+  {//otherwise check it is already a seq object
+    if(typeof(rna_protein) !== "object"){
+      throw("protein must be object");
+    }
+    if(!(rna_protein && Object.keys(rna_protein).length === 0 && rna_protein.constructor === Object))
+    {
+      if(! rna_protein.identity || rna_protein.identity !== 'protein')
+      {
+        throw("Input protein object must have protein identity");
+      }
+    }
+  }
+
+  if(typeof rna_sequence === 'string' || rna_sequence instanceof String)
+  {//if we got a string try and make a sequence out of it
+     rna_sequence = sequence.sequence(rna_sequence, 'nucleotide');
+  }
+  else
+  {//otherwise check it is already a seq object
+    if(typeof(rna_sequence) !== "object"){
+      throw("rna_sequence must be object");
+    }
+    if(! rna_sequence.identity || rna_sequence.identity !== 'sequence')
+    {
+       throw("Input rna_sequence object must have sequence identity");
+    }
+  }
+
+  if(! (typeof source === 'string' || source instanceof String))
+  {
+     throw("rna source must be a string");
+  }
+  let prot_data = {
+    identity: "rna",
+    type: type,
+    sequence: rna_sequence,
+    protein: rna_protein,
+    source: source,
+    annotations: annotations,
+  };
+  return(prot_data);
+};
+
+module.exports = {
+  rna: rna,
+};
---
+++ test/rna.test.js
@@ -0,0 +1,53 @@
+/*jshint esversion: 6 */
+const expect = require('chai').expect;
+let bd = require('../rna.js');
+let bds = require('../sequence.js');
+let bdp = require('../protein.js');
+
+///////////////
+// Fixtures //
+//////////////
+seq_annotations = {GO: ["GO:000012", "GO:1223423"]};
+source = "file.csv";
+prot = bdp.protein('ARNBD');
+rnaseq = bds.sequence('ATUG', "nucleotide");
+rna = bd.rna(rnaseq);
+rna_full = bd.rna('ATUG', prot, seq_annotations, source);
+rna_prot = bd.rna('ATUG', "PPP", seq_annotations, source, "trna");
+
+///////////
+// Tests //
+///////////
+describe('RNA: general sequence generation', () => {
+  it('should raise invalid rna_sequence types', () => {
+    expect(() => bd.rna(2)).to.throw("rna_sequence must be object");
+    expect(() => bd.rna([])).to.throw("Input rna_sequence object must have sequence identity");
+  });
+
+  it('rna should have a string source', () => {
+    expect(() => bd.rna("ATUG", undefined, undefined, 123)).to.throw("rna source must be a string");
+  });
+  it('rna should have empty annotations', () => {
+      expect(rna.source).to.be.empty;
+  });
+  it('rna should have empty annotations', () => {
+      expect(rna.annotations).to.be.empty;
+  });
+  it('rna should have populated annotations', () => {
+      expect(rna_full.annotations).to.eql(seq_annotations);
+  });
+  it('rna should have populated source', () => {
+      expect(rna_full.source).to.eql(source);
+  });
+  it('rna should make a sequence object when given a string', () => {
+      expect(rna_full.sequence.identity).to.equal("sequence");
+  });
+  it('rna should make a nucleotide sequence object when given a string', () => {
+      expect(rna_full.sequence.type).to.equal("nucleotide");
+  });
+
+  it('rna should make a protein object when given protein seq', () => {
+      expect(rna_prot.protein.identity).to.equal("protein");
+  });
+
+});
